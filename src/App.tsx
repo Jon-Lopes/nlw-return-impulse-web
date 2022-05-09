c9b8f932227e7de7a485cdc87488c8d5c08bae14
Jonatas S Lopes <jonatas.siq.lopes@gmail.com>
2022-05-09 09:16:55 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Widget } from "./Components/Widget";
+
+export function App(){
+  return <Widget />;
+}
+
+
+
+
+
+
+
+// interface BtnProps {
+//   text?: string;
+// }
+// function Btn(props: BtnProps) {
+//   return <button className="bg-slate-500 px-4 h-10 rounded text-slate-100 hover:bg-slate-700 transition-colors">{props.text ?? 'Default'}</button>;
+// }
+// function MockApp() {
+//   return (
+//     <div className="flex gap-3">
+//       <Btn text="Hello"/>
+//       <Btn text="Welcome"/>
+//       <Btn text="Register"/>
+//       <Btn text="Login"/>
+//       <Btn text="Log"/>
+//       <Btn text= "" />
+//       <Btn />
+//     </div>)
+// }
+// export default App
